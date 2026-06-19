d9b21794c211b9b9359ad0d8e8ac77e6deb7e7bf
felipemerlin <felipe.simao@edu.senai.br>
2026-06-19 11:11:59 -0300

    Questões para a prova

--- questao15.js
+++ questao15.js
@@ -1,7 +1,7 @@
 // Crie um script em JavaScript para classificar a eficiência energética de um motor elétrico industrial. O script lerá a variável consumoKwm e o fatorPotencia. Se o fator de potência fr menor que 0.92, imprima "Alerta: Fator de Potência Crítico". Se for normal (=> 0.92), analise o consumo: consumo abaixo de 500 kWh ganha classificação "Selo Verde", entre 500 e 1000 kWh classificação "Selo Amarelo", e cima de 1000 kWh classificação "Selo Vermelho".
 
-let consumoKwm = 750; // Exemplo de consumo em kWh
-let fatorPotencia = 0.95; // Exemplo de fator de potência
+let consumoKwm = 750;
+let fatorPotencia = 0.95;
 
 if (fatorPotencia < 0.92) {
     console.log("Alerta: Fator de Potência Crítico");
